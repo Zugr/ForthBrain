f6dd9e1d23e13f9c720359f11a032ef0d06bb7ec
Сушков Григорий <zugr.77@gmail.com>
2016-05-14 10:42:48 +0300

    Созданиепервичной архитектуры каталогов

---
+++ src/scripts/xx.js
@@ -0,0 +1,6 @@
+
+function ss(c) {
+	if (c>1)
+		return 15;
+	return 0;
+}
---
+++ src/scripts/xx.spec.js
@@ -0,0 +1,6 @@
+describe('first', function() {
+	it('a', function() {
+		expect(ss(10)).toBe(15);
+		expect(ss(0)).toBe(0);
+	})
+})
